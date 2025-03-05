502571c1152c933d41ab57ac4d6aeb5337aaaaaa
atmhusky <atsumi820@gmail.com>
2025-03-06 04:01:34 +0900

    refactor: 検索部分と本のアイテムを分割

--- MyLibrary/Views/ListView.swift
+++ MyLibrary/Views/ListView.swift
@@ -19,44 +19,12 @@ struct ListView: View {
                 
                 List(selection: $selectedItems) {
                     Section("本の追加") {
-                        HStack {
-                            TextField("ISBNコードを直接入力", text: $searchText)
-                                .onSubmit {
-                                    print(searchText)
-                                    isShowBookDetailView = true
-                                }
-                            
-                            Button {
-                                print("camera")
-                            } label: {
-                                Image(systemName: "barcode.viewfinder")
-                                    .font(.title2)
-                            }
-                        }
+                        search
                     }
                     
                     Section(header: Text("蔵書一覧")){
                         ForEach(1..<10, id: \.self) { item in
-                            NavigationLink {
-                                BookDetailView()
-                            } label: {
-                                HStack {
-                                    Image("no_image")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 50, height: 80)
-                                    
-                                    VStack(alignment: .leading) {
-                                        Text("本のタイトル")
-                                            .font(.body)
-                                            .lineLimit(1)
-                                        
-                                        Text("本の著者")
-                                            .font(.caption)
-                                            .foregroundStyle(.secondary)
-                                    }
-                                }
-                            }
+                            bookItem
                         }
                     }
                 }
@@ -65,14 +33,14 @@ struct ListView: View {
                 .navigationTitle("MyLibraty")
                 .toolbar {
                     ToolbarItem(placement: .topBarTrailing) {
-                        Button(editMode == .active ? "完了" : "編集") {
+                        Button(editMode == .active ? "完了" : "選択") {
                             editMode = editMode == .active ? .inactive : .active
                         }
                     }
                     ToolbarItem(placement: .topBarLeading) {
                         if editMode == .active {
                             Button("すべて選択") {
-                               selectAllItems()
+                                selectAllItems()
                             }
                         }
                     }
@@ -80,20 +48,24 @@ struct ListView: View {
                     ToolbarItemGroup(placement: .bottomBar) {
                         if editMode == .active {
                             Spacer()
+                            
                             Button {
                                 exportItems()
                             } label: {
                                 Image(systemName: "square.and.arrow.up")
                             }
+                            
                             Spacer()
+                            
                             Button {
                                 removeItems()
                             } label: {
                                 Image(systemName: "trash")
                                     .foregroundStyle(.red)
                             }
+                            
                             Spacer()
-        
+                            
                         }
                     }
                 }
@@ -112,6 +84,46 @@ struct ListView: View {
 }
 
 extension ListView {
+    
+    private var search: some View {
+        HStack {
+            TextField("ISBNコードを直接入力", text: $searchText)
+                .onSubmit {
+                    print(searchText)
+                    isShowBookDetailView = true
+                }
+            
+            Button {
+                print("camera")
+            } label: {
+                Image(systemName: "barcode.viewfinder")
+                    .font(.title2)
+            }
+        }
+    }
+    
+    private var bookItem: some View {
+        NavigationLink {
+            BookDetailView()
+        } label: {
+            HStack {
+                Image("no_image")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 50, height: 80)
+                
+                VStack(alignment: .leading) {
+                    Text("本のタイトル")
+                        .font(.body)
+                        .lineLimit(1)
+                    
+                    Text("本の著者")
+                        .font(.caption)
+                        .foregroundStyle(.secondary)
+                }
+            }
+        }
+    }
     
     private func removeItems() {
         print("削除するアイテム: \(selectedItems)")
